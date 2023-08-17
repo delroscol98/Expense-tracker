857bb38fbbd47b249f3f03d78baedfeefe3c9a6b
delroscol98 <collindelrosario98@gmail.com>
2023-08-17 22:31:49 +1000

    stored user input into object

--- src/components/Expenses/newExpense/ExpenseForm.js
+++ src/components/Expenses/newExpense/ExpenseForm.js
@@ -70,8 +70,20 @@ const ExpenseForm = () => {
   //     if (identifier === "date") setEnteredDate(value);
   //   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <div>Title</div>
