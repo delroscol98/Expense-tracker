1abbafa012d90886dad5ad40f4952e8249cafc2b
delroscol98 <collindelrosario98@gmail.com>
2023-08-21 23:16:18 +1000

    added dynamic expense chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
+
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+
 import Card from "../UI/Card";
+
 import "./Expenses.css";
 
 const Expenses = (props) => {
@@ -22,6 +26,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
